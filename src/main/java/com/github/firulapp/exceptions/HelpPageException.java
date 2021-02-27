39e67968a25aa4e7f78e480fe2a6ce66a055c302
Jazmín Villagra <jazvillagra.jv@gmail.com>
2021-02-27 13:10:06 -0300

    Agrega excepciones customizadas

---
+++ src/main/java/com/github/firulapp/exceptions/HelpPageException.java
@@ -0,0 +1,29 @@
+package com.github.firulapp.exceptions;
+
+import java.util.function.Supplier;
+
+public class HelpPageException extends BusinessException implements Supplier<HelpPageException>{
+
+        private static final long serialVersionUID = 1L;
+        public static final String BASE_ERROR = "helpPage.error";
+        public static final String NOT_FOUND = BASE_ERROR + ".notFound";
+
+    public HelpPageException(String errorCode, String message) {
+        super(errorCode, message);
+    }
+
+        /**
+         * Builds a Help Page Not Found Exception
+         *
+         * @param id Help Page id
+         * @return HelpPageException
+         */
+        public static HelpPageException notFound(Long id) {
+        return new HelpPageException(NOT_FOUND, String.format("Página de ayuda con id %n no encontrado" , id));
+    }
+
+        @Override
+        public HelpPageException get() {
+        return null;
+    }
+}
