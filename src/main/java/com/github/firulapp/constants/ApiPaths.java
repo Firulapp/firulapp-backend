5e12dade001c65b3a2634f8b2cf68a84f2d54b7d
Jazmín Villagra <jazvillagra.jv@gmail.com>
2021-03-03 01:12:31 -0300

    Agrega urls para endpoints de parámetros

--- src/main/java/com/github/firulapp/constants/ApiPaths.java
+++ src/main/java/com/github/firulapp/constants/ApiPaths.java
@@ -6,11 +6,33 @@ public class ApiPaths {
     public static final String BASE_URL = "/api";
 
     /**
-     * User enpoints' URLs
+     * User endpoints' URLs
      */
     public static final String USER_ENDPOINTS_URL = BASE_URL + "/user";
     public static final String LOGIN_URL = "/login";
     public static final String REGISTER_URL = "/register";
     public static final String LOGOUT_URL = "/logout";
     public static final String GET_BY_TYPE = "/type";
+
+    /**
+     * Params endpoints' URLs
+     */
+    public static final String PARAM_ENDPOINTS_URL = BASE_URL + "/param";
+    public static final String SPECIES_ENDPOINTS = "/species";
+    public static final String SPECIES_BY_ID = SPECIES_ENDPOINTS + "/id/{id}";
+    public static final String SPECIES_SAVE = SPECIES_ENDPOINTS + "/save";
+    public static final String BREED_ENDPOINTS = "/breed";
+    public static final String BREED_SAVE = BREED_ENDPOINTS + "/save";
+    public static final String BREED_BY_ID = BREED_ENDPOINTS + "/id/{id}";
+    public static final String BREED_BY_SPECIES_ID = BREED_ENDPOINTS + SPECIES_BY_ID;
+    public static final String CONDUCT_RULE_ENDPOINTS = "/rules";
+    public static final String CONDUCT_RULE_BY_ID = CONDUCT_RULE_ENDPOINTS + "/id/{id}";
+    public static final String CONDUCT_RULE_SAVE = CONDUCT_RULE_ENDPOINTS + "/save";
+    public static final String HELP_PAGE_ENDPOINTS = "/help";
+    public static final String HELP_PAGE_BY_ID = HELP_PAGE_ENDPOINTS + "/id/{id}";
+    public static final String HELP_PAGE_SAVE = HELP_PAGE_ENDPOINTS + "/save";
+    public static final String SERVICE_TYPE_ENDPOINTS = "/service/type";
+    public static final String SERVICE_TYPE_BY_ID = SERVICE_TYPE_ENDPOINTS + "/id/{id}";
+    public static final String SERVICE_TYPE_SAVE = SERVICE_TYPE_ENDPOINTS + "/save";
+
 }
