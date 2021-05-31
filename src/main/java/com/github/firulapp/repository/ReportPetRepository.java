aa385f97ad589d83d93159f9412eb66578fb5dd2
Jazmín Villagra <jazvillagra.jv@gmail.com>
2021-05-31 17:02:48 -0400

    Agrega servicios para obtener y guardar reportes de mascotas

---
+++ src/main/java/com/github/firulapp/repository/ReportPetRepository.java
@@ -0,0 +1,29 @@
+package com.github.firulapp.repository;
+
+import com.github.firulapp.domain.ReportPet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+public interface ReportPetRepository extends JpaRepository<ReportPet, Long> {
+
+    @Query(value = "SELECT * FROM reporte_mascota rp " +
+            "WHERE rp.fecha_creacion BETWEEN :startDate AND :endDate",
+            nativeQuery = true)
+    List<ReportPet> findByCreatedAt(@Param("startDate") LocalDateTime startDate,
+                                    @Param("endDate") LocalDateTime endDate);
+
+    @Query(value = "SELECT * FROM reporte_mascota rp " +
+            "WHERE (rp.latitud_ubicacion BETWEEN :latitudeMin AND :latitudeMax) " +
+                "AND (rp.longitud_ubicacion BETWEEN :longitudeMin AND :longitudeMax)",
+            nativeQuery = true)
+    List<ReportPet> findByLocationLatitudeAndLocationLongitude(@Param("latitudeMin") Double latitudeMin,
+                                             @Param("longitudeMin") Double longitudeMin,
+                                             @Param("latitudeMax") Double latitudeMax,
+                                             @Param("longitudeMax") Double longitudeMax);
+}
