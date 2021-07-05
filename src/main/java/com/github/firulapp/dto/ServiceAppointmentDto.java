3b72901791cfae68715eb981b9d90e0bd6465d6e
Jazmín Villagra <jazvillagra.jv@gmail.com>
2021-07-05 11:03:14 -0400

    Agrega objetos para turnos de servicios

---
+++ src/main/java/com/github/firulapp/dto/ServiceAppointmentDto.java
@@ -0,0 +1,100 @@
+package com.github.firulapp.dto;
+
+import com.github.firulapp.constants.AppointmentStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ServiceAppointmentDto {
+
+    private Long id;
+    private Long serviceId;
+    private Long userId;
+    private Long petId;
+    private LocalDate appointmentDate;
+    private AppointmentStatus status;
+    private LocalDateTime createdAt;
+    private Long createdBy;
+    private LocalDateTime modifiedAt;
+    private Long modifiedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Long petId) {
+        this.petId = petId;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public AppointmentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AppointmentStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
